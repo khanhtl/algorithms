8e8e03c590939ac5f9ef6a10ccef146ae80ffdda
tlkhanh <longkhanhta@gmail.com>
2023-11-20 23:42:38 +0700

    array data structure

---
+++ array-data-structure/app.js
@@ -0,0 +1,11 @@
+const a = new ArrayBuffer(6);
+console.log(a);
+const a8 = new Uint8Array(a);
+a8[0] = 10;
+console.log(a);
+a8[2] = 10;
+console.log(a);
+const a16 = new Uint16Array(a);
+console.log(a);
+a16[2] = 0x1010;
+console.log(a);
